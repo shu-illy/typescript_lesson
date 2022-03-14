3cc43d4680ca97071f35058630601e041f3e40af
shu-illy <sh.illy.9117@gmail.com>
2022-03-15 00:24:32 +0900

    lesson15

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Data from "./data.json";
 
-const name = "hello"
+// JSONの型推論
+// typeofを使うことで、JSONのkeyをまとめて型定義することができる
+type USERS = typeof Data;
 
-let username: string = "Hello"
+const name = "hello";
+
+let username: string = "Hello";
 let dummyNum = 2;
 
-let array2 = [0, 2, "hello"]
+let array2 = [0, 2, "hello"];
 
 interface NAME {
   first: string;
@@ -21,18 +26,18 @@ let nambeObj: NAME = {
 
 const func1 = (x: number, y: number): number => {
   return x + y;
-}
+};
 
 // Intersection Types
 type PROFILE = {
   age: number;
   city: string;
-}
+};
 
 type LOGIN = {
   username: string;
   password: string;
-}
+};
 
 type USER = PROFILE & LOGIN;
 
@@ -41,21 +46,21 @@ const userA: USER = {
   city: "Tokyo",
   username: "xxx",
   password: "yyy"
-}
+};
 
 // Union Types
-let value: boolean | number
+let value: boolean | number;
 value = true;
 value = 10;
 
 let arrayUni: (number | string)[];
 arrayUni = [0, 1, 2, 3, "hello"];
 
-let company: "Facebook" | "Google" | "Amazon"
+let company: "Facebook" | "Google" | "Amazon";
 company = "Amazon";
 
 let memory: 256 | 512;
-memory = 512
+memory = 512;
 
 // typeof
 
@@ -65,14 +70,14 @@ let msg2: typeof msg = "hello";
 let animal = { cat: "small cat" };
 let newAnimal: typeof animal = {
   cat: "big cat",
-}
+};
 
 // keyof
 type KEYS = {
   primary: string;
   secondary: string;
-}
-let key: keyof KEYS
+};
+let key: keyof KEYS;
 key = "primary";
 
 // typeof + keyof
@@ -92,18 +97,80 @@ enum OS {
 
 interface PC {
   id: number,
-  OSType: OS
+  OSType: OS;
 }
 
 const PC1: PC = {
   id: 1,
   OSType: OS.Windows
-}
+};
 
 const PC2: PC = {
   id: 2,
   OSType: OS.Mac
+};
+
+// 型の互換性
+
+const comp1 = "test";
+let comp2: string = comp1;
+
+let comp3: string = "test";
+
+let funcComp1 = (x: number) => { };
+let funcComp2 = (x: string) => { };
+
+// funcComp1 = funcComp2;
+// funcComp2 = funcComp1;
+
+// Generics
+interface GEN<T> {
+  item: T;
 }
+const gen0: GEN<string> = { item: "hello" };
+// const gen1: GEN = {item : "hello"};
+const gen1: GEN<number> = { item: 12 };
+
+interface GEN1<T = string> {
+  item: T;
+}
+const gen3: GEN1 = { item: "hello" };
+
+interface GEN2<T extends string | number> {
+  item: T;
+}
+
+const gen4: GEN2<string> = {
+  item: "hoge"
+};
+
+function funcGen<T>(props: T) {
+  return { item: props };
+}
+const gen6 = funcGen("test");
+const gen7 = funcGen<string | null>(null);
+
+function funcGen1<T extends string | null>(props: T) {
+  return { value: props };
+}
+
+const gen8 = funcGen1("hello");
+// const gen9 = funcGen1(11);
+
+interface Props {
+  price: number;
+}
+function funcGen3<T extends Props>(props: T) {
+  return { value: props.price };
+}
+
+const gen10 = funcGen3({ price: 10 });
+
+const funcGen4 = <T extends Props>(props: T) => {
+  return { value: props.price };
+};
+
+
 
 function App() {
   return (
